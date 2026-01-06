283a9b72930fa72efc89dd923704b015799ee24b
kinjal24jibca025-beep <kinjal.24jibca025@jietjodhpur.ac.in>
2026-01-06 14:11:44 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float add(float a, float b){
+    return a + b;
+}
+
+float subtract(float a, float b){
+    return a - b;
+}
+
+float multiply(float a, float b){
+    return a * b;
+}
+
+float divide(float a, float b){
+    if(b == 0){
+        return 0;
+    }
+    return a / b;
+}
